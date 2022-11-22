0c2af2ad6fc17da9865b101f09db5ab96f4df6a0
Yasmim Ferreira Costa <97356148+yasmimferreir@users.noreply.github.com>
2022-11-22 11:21:56 -0400

    projetologin

--- src/App.js
+++ src/App.js
@@ -3,7 +3,47 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1> Reactjs Projeto</h1>
+      <div className='container-global'>
+        <div className='container'>
+          <h2 className='title'>Bem vindos ao<span> Login</span></h2>
+          <p className='paragrafh'>Preencha os dados do login para acessar</p>
+          <div className='register--input'>
+
+            <p className='principal'>Usuário
+              <br />
+              <input
+                id='nome'
+                className='input--usuario'
+                type="email"
+                placeholder="usuario"
+                required
+              />
+            </p>
+            <hr className='linha' />
+
+
+            <p className='principal'>Senha
+              <br />
+              <input
+                id='password'
+                className='input--usuario'
+                type="password"
+                placeholder="senha"
+                required
+              />
+            </p>
+            <hr className='linha' />
+
+            <div className='container--button'>
+              <button className='button'>
+                Entrar
+              </button>
+            </div>
+
+
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
